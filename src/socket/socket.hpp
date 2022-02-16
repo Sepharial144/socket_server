a87e3e27cfd04bb23224f11cf0dbe0fa5827a296
Dmitry <ivaschenkoda144@gmail.com>
2022-02-16 16:04:03 +0600

    reforce socket class definition

--- src/socket/socket.hpp
+++ src/socket/socket.hpp
@@ -20,31 +20,96 @@ namespace Net {
 		WSADATA _wsaData;
 		int _initStatus;
 
-		int _bufferLength;
+		int _bufferLen;
 		char* _defaultPort;
 
-		SOCKET _listenSocket = INVALID_SOCKET;
+		SOCKET _listenSocket = INVALID_cleaSOCKET;
 		SOCKET _clientSocket = INVALID_SOCKET;
 
 		struct addrinfo* _ptrInfo;
 		struct addrinfo _hints;
 
-		int iSendResult;
+		int _sendStatus;
 		char* _recvbuf;
-
-		int Init();
-
-		void SetSocketParameters();
-
-		int CreateSocket();
-
-		int ListenSocket();
-
+	
 	public:
-		Socket(int buf_len, char* default_port);
-		~Socket();
 
-	};
+		Socket::Socket(int buf_len, char* default_port):
+		_bufferLen(buf_len),
+		_defaultPort(default_port),
+		_listenSocket(INVALID_SOCKET),
+		_clientSocket(INVALID_SOCKET),
+		_ptrInfo(nullptr)
+		{
+			assert(_bufferLength > 0 && _bufferLength <= Settings::tcp_ip_packet_max_size);
+
+			_recvbuf = new char[_bufferLen];
+		}
+
+	Socket::~Socket()
+	{
+		freeaddrinfo(_ptrInfo);
+		delete[] _recvbuf;
+	}
+		int Socket::Init() {
+
+			_initStatus = WSAStartup(MAKEWORD(2, 2), &_wsaData);
+			if (_initStatus != 0) {
+				//m_Error = "WSAStartup failed with error: ";
+				//Logging::Logger::WriteLog(Logging::SOCKET, Logging::ERROR_MESSAGE, strcat(m_Error, static_cast<char*>(_initStatus)));
+				printf("WSAStartup failed with error: %d\n", _initStatus);
+				return 0;
+			}
+			else {
+				printf("WSAStartup started: %d\n", _initStatus);
+			}
+		}
+
+	void Socket::SetSocketParameters()
+	{
+		ZeroMemory(&_hints, sizeof(_hints));
+		_hints.ai_family = Addrinfo::aifamily::FAM_AF_INET;
+		_hints.ai_socktype = Addrinfo::aisocktype::TYPE_SOCK_STREAM;
+		_hints.ai_protocol = Addrinfo::aiprotocol::PROTOCOL_TCP;
+		_hints.ai_flags = Addrinfo::aiflags::FLAG_AI_PASSIVE;
+	}
+
+	int Socket::CreateSocket() 
+	{
+
+		SetSocketParameters();
+
+		// Resolve the server address and port
+		_initStatus = getaddrinfo(NULL, _defaultPort, &_hints, &_ptrInfo);
+		if (_initStatus != 0) {
+
+			printf("getaddrinfo failed with error: %d\n", _initStatus);
+			WSACleanup();
+			return 0;
+		}
+		else {
+			printf("GetaddrInfo successfully.\n");
+		}
+		return 1;
+	}
+
+	int Socket::ListenSocket()
+	{
+		// Listen socket for connecting to server
+		_listenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+		if (_listenSocket == INVALID_SOCKET) {
+			printf("socket failed with error: %ld\n", WSAGetLastError());
+			freeaddrinfo(result);
+			WSACleanup();
+			return 1;
+		}
+		else {
+
+			std::cout << "Listenning successfully.\n";
+		}
+		return 0;
+	}
 
 } // namespace Net
 
