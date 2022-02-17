db61ed13b5b106995e2f8dd331e19823785ee0aa
Dmitry <ivaschenkoda144@gmail.com>
2022-02-17 18:33:32 +0600

    ad logger

--- src/socket/socket.hpp
+++ src/socket/socket.hpp
@@ -3,6 +3,9 @@
 
 class Socket
 {
+
+    virtual int Guard(int socket_fd, const char* message);
+
     virtual void init();
 
     virtual void setParameter();
@@ -16,11 +19,53 @@ class Socket
     virtual void accept();
 };
 
+#include <memory>
+#include <iostream>
+
+class Logger
+{
+
+    public:
+    Logger() {};
+    
+    void warning(const char* module, const char* message) {
+        std::cout << message << "\n";
+    };
+};
+
+using ptrLogger = std::shared_ptr<Logger>;
+
 class ServerSocket: Socket
 {
     private:
+
+        ptrLogger _logger;
+
         int _socketServer_fd;
+
+    private:
+        int Guard(int socket_fd, const char* message) override
+        {
+            if (socket_fd == -1)
+            {
+                _logger->warning("Socket server", message);
+            }
+            return socket_fd;
+        }
+
+        void init() override
+        {
+        
+        }
+
+
+
     public:
+        ServerSocket(std::shared_ptr<Logger> logger):
+        _logger(logger)
+        {
+            init();
+        };
 
 }
 
